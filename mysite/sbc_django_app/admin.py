2c4b544a364c14a15bd735580b7a4fd13b28b3d0
a1tdev <107887422+a1tdev@users.noreply.github.com>
2024-11-04 15:21:21 +0000

    First django commit

---
+++ mysite/sbc_django_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Task  # Replace with your model name
+
+admin.site.register(Task)
---
+++ mysite/sbc_django_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SbcDjangoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sbc_django_app'
